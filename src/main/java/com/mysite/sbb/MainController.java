7ca6291dfa15a02df43ad407c4a2cc42c9175924
Park-seungg <parkis1101@gmail.com>
2025-08-14 09:44:48 +0900

    @ResponseBody 로 응답 받기

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,12 +2,15 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
+    @ResponseBody
     @GetMapping("/")
-    public void index(){
-        System.out.println("index 메서드 호출됨!");
+    public String index(){
+        return "sbb";
     }
 
     @GetMapping("/hello")
