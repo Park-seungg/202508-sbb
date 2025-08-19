1e83a1659e34c13bbffd8593abcb1a3010b1720d
Park-seungg <parkis1101@gmail.com>
2025-08-19 10:16:16 +0900

    루트 URL 사용

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,15 +2,13 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @ResponseBody
     @GetMapping("/")
-    public String index(){
-        return "sbb";
+    public String index() {
+        return "redirect:/question/list";
     }
 
     @GetMapping("/hello")
